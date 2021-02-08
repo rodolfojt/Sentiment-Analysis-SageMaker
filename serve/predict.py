7fc5bdef9eaf06b271aca82d506200445052dbc5
Rodolfo Teles <rodolfojeronimoteles@gmail.com>
2021-02-08 18:10:34 -0300

    Cleaning predict.py

--- serve/predict.py
+++ serve/predict.py
@@ -90,7 +90,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
     
-#     test_X = pd.DataFrame(data_pack)
     predictions = model(data)
     predictions = round(float(predictions.detach().numpy()))
     
